8d95f64fb89985afb2f9571b146fdd24680a0a56
Pragnesh-koli <pragnesh.softcolon@gmail.com>
2025-01-31 19:37:34 +0530

    permission added

--- lib/flutter_call_handler.dart
+++ lib/flutter_call_handler.dart
@@ -1,12 +1,29 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FlutterCallHandler {
   static const MethodChannel _channel =
       MethodChannel('com.example.followclient/call_handler');
 
   static Future<void> makeCall(String phoneNumber) async {
-    await _channel.invokeMethod('makeCall', {'phoneNumber': phoneNumber});
+    try {
+      final PermissionStatus permissionStatus =
+      await Permission.phone.request();
+
+      if (permissionStatus.isGranted) {
+        try {
+          await _channel.invokeMethod('makeCall', {'phoneNumber': phoneNumber});
+        } on PlatformException catch (e) {
+          print('Failed to make call: ${e.message}');
+        }
+      } else {
+        throw 'Phone permission not granted';
+      }
+    } catch (e) {
+      print('Failed to make call: ${e.toString()}');
+    }
+
   }
 }
