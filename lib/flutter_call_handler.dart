010542c133f3cb1ccca5b75226aa8dd895c1eca3
Pragnesh-koli <pragnesh.softcolon@gmail.com>
2025-02-28 09:41:14 +0530

    permission handler removed

--- lib/flutter_call_handler.dart
+++ lib/flutter_call_handler.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class FlutterCallHandler {
   static const MethodChannel _channel =
@@ -9,18 +8,11 @@ class FlutterCallHandler {
 
   static Future<void> makeCall(String phoneNumber) async {
     try {
-      final PermissionStatus permissionStatus =
-      await Permission.phone.request();
-
-      if (permissionStatus.isGranted) {
         try {
           await _channel.invokeMethod('makeCall', {'phoneNumber': phoneNumber});
         } on PlatformException catch (e) {
           print('Failed to make call: ${e.message}');
         }
-      } else {
-        throw 'Phone permission not granted';
-      }
     } catch (e) {
       print('Failed to make call: ${e.toString()}');
     }
